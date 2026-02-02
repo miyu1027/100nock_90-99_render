d8e802c2a94b9dd15acbc4a1de7a9dc780bdee47
miyu1027 <miyu151027@gmail.com>
2026-02-03 02:45:52 +0900

    Add multiple endpoints

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    const msg = process.env.MESSAGE || "No env variable set";
-    res.send(`ENV MESSAGE: ${msg}`);
+    res.send("Top page");
+});
+
+app.get("/api/hello", (req, res) => {
+    res.json({ message: "Hello API!" });
+});
+
+app.get("/about", (req, res) => {
+    res.send("This is the about page.");
 });
 
 app.listen(PORT, () => {
