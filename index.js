3031c684015fc7fcb3bb538be6c0533f9b72dad7
miyu1027 <miyu151027@gmail.com>
2026-02-03 02:25:31 +0900

    Read env var from Render

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, Render! Auto deploy works!");
+    const msg = process.env.MESSAGE || "No env variable set";
+    res.send(`ENV MESSAGE: ${msg}`);
 });
 
 app.listen(PORT, () => {
