5fe87bb19b05ee4667abecf9f1bd9f683f55e7b2
miyu1027 <miyu151027@gmail.com>
2026-02-03 02:08:33 +0900

    Update message for auto deploy test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, Render!");
+    res.send("Hello, Render! Auto deploy works!");
 });
 
 app.listen(PORT, () => {
